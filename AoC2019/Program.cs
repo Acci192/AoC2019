ab3922d37307426c179f3c9aa51da3e9a1fa7686
Acci192 <k.andreas@live.se>
2019-12-01 08:20:22 +0100

    Day 1

---
+++ AoC2019/Program.cs
@@ -0,0 +1,29 @@
+using AoC2019.Solutions;
+using System;
+
+namespace AoC2019
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string> methodToRun = Day1.B;
+            var input = System.IO.File.ReadAllText($"../../Inputs/{methodToRun.Method.DeclaringType?.Name}.txt");
+
+
+            var result = methodToRun(input);
+
+            Console.WriteLine(result);
+
+            Console.Write("\nIf you want to test the execution time of the current method, enter a number of iterations: ");
+            var readLine = Console.ReadLine();
+
+            if (!int.TryParse(readLine, out var numIterations)) return;
+
+            Benchmark.DisplayTimerProperties();
+            Console.WriteLine();
+            Benchmark.TimeOperations(methodToRun, input, numIterations);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AoC2019/Solutions/Day1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2019.Solutions
+{
+    public class Day1
+    {
+        public static string A(string input)
+        {
+            return input.Split('\n').Select(int.Parse).Sum(x => x/3-2).ToString();
+        }
+
+        public static string B(string input)
+        {
+            return input.Split('\n').Select(int.Parse).Select(x => x / 3 - 2).Select(FuelCounter).Sum().ToString();
+        }
+
+        private static int FuelCounter(int moduleWeight)
+        {
+            var result = 0;
+            while (moduleWeight > 0)
+            {
+                result += moduleWeight;
+                moduleWeight = moduleWeight / 3 - 2;
+            }
+            return result;
+        }
+    }
+}
