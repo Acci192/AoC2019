3f962cf92a7b8cf50b30975e8ceb4d7bd3d84244
Acci192 <k.andreas@live.se>
2019-12-03 19:06:00 +0100

    Day 3

--- AoC2019/Solutions/Day3.cs
+++ AoC2019/Solutions/Day3.cs
@@ -10,14 +10,102 @@ namespace AoC2019.Solutions
     {
         public static string A(string input)
         {
-            var rows = input.Split('\n').ToList();
-            return string.Empty;
+            var wires = input.Split('\n').Select(x => x.Split(',')).ToList();
+            var occopiedSpace = new HashSet<Tuple<int, int>>();
+
+            var shortestPath = int.MaxValue;
+            for (var i = 0; i < 2; i++)
+            {
+                var curPosX = 0;
+                var curPosY = 0;
+                for (var j = 0; j < wires[i].Count(); j++)
+                {
+                    var move = wires[i][j];
+                    var direction = move.First();
+                    var amount = int.Parse(string.Join("", move.Skip(1).ToList()));
+                    for (var k = 0; k < amount; k++)
+                    {
+                        switch (direction)
+                        {
+                            case 'R':
+                                curPosX++;
+                                break;
+                            case 'L':
+                                curPosX--;
+                                break;
+                            case 'U':
+                                curPosY--;
+                                break;
+                            case 'D':
+                                curPosY++;
+                                break;
+                        }
+                        if(i == 0)
+                        {
+                            occopiedSpace.Add(new Tuple<int, int>(curPosX, curPosY));
+                        }
+                        else if(occopiedSpace.Contains(new Tuple<int, int>(curPosX, curPosY)))
+                        {
+                                var dist = Math.Abs(curPosX) + Math.Abs(curPosY);
+                                if (dist < shortestPath)
+                                    shortestPath = dist;
+                        }
+                    }
+                }
+            }
+            return shortestPath.ToString();
         }
 
         public static string B(string input)
         {
-            var rows = input.Split('\n').ToList();
-            return string.Empty;
+            var rows = input.Split('\n').Select(x => x.Split(',')).ToList();
+            var occopiedSpace = new Dictionary<Tuple<int, int>, int>();
+            var grid = new char[250, 250];
+
+            var shortestPath = int.MaxValue;
+            for (var i = 0; i < 2; i++)
+            {
+                var curPosX = 0;
+                var curPosY = 0;
+                var counter = 0;
+                for (var j = 0; j < rows[i].Count(); j++)
+                {
+                    var move = rows[i][j];
+                    var direction = move.First();
+                    var amount = int.Parse(string.Join("", move.Skip(1).ToList()));
+                    for (var k = 0; k < amount; k++)
+                    {
+                        counter++;
+                        switch (direction)
+                        {
+                            case 'R':
+                                curPosX++;
+                                break;
+                            case 'L':
+                                curPosX--;
+                                break;
+                            case 'U':
+                                curPosY--;
+                                break;
+                            case 'D':
+                                curPosY++;
+                                break;
+                        }
+                        if (i == 0)
+                        {
+                            if(!occopiedSpace.ContainsKey(new Tuple<int, int>(curPosX, curPosY)))
+                                occopiedSpace.Add(new Tuple<int, int>(curPosX, curPosY), counter);
+                        }
+                        else if (occopiedSpace.ContainsKey(new Tuple<int, int>(curPosX, curPosY)))
+                        {
+                            var dist = occopiedSpace[new Tuple<int, int>(curPosX, curPosY)] + counter;
+                            if (dist < shortestPath)
+                                shortestPath = dist;
+                        }
+                    }
+                }
+            }
+            return shortestPath.ToString();
         }
     }
 }
---
+++ AoC2019/Solutions/DayTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2019.Solutions
+{
+    public class DayTemplate
+    {
+        public static string A(string input)
+        {
+            var rows = input.Split('\n');
+            return string.Empty;
+        }
+
+        public static string B(string input)
+        {
+            var rows = input.Split('\n');
+            return string.Empty;
+        }
+    }
+}
