22b4d82ab25e47cd7a346741a1915c6a91ae7b9b
Acci192 <k.andreas@live.se>
2019-12-15 08:14:09 +0100

    Day14a and Day15

--- AoC2019/Solutions/Day14.cs
+++ AoC2019/Solutions/Day14.cs
@@ -1,12 +1,189 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AoC2019.Solutions
 {
-    class Day14
+    public class Day14
     {
+        public static long UsedOre = 0;
+        public static string A(string input)
+        {
+            UsedOre = 0;
+            var rows = input.Replace("\r", "").Split('\n');
+
+            var nodes = new Dictionary<string, int>();
+            var connections = new List<Connection>();
+
+            foreach(var row in rows)
+            {
+                var split = row.Split(new[] { "=>" }, StringSplitOptions.RemoveEmptyEntries);
+                var froms = split[0].Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                var toInfo = split[1].Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var toNode = toInfo[1];
+                var toAmount = int.Parse(toInfo[0]);
+                nodes[toNode] = 0;
+                foreach(var from in froms)
+                {
+                    var fromInfo = from.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    var fromNode = fromInfo[1];
+                    var fromAmount = int.Parse(fromInfo[0]);
+                    nodes[fromNode] = 0;
+                    connections.Add(new Connection(fromNode, toNode, fromAmount, toAmount));
+                }
+            }
+
+            GetAmountAtNode("FUEL", 1, nodes, connections);
+            
+            return UsedOre.ToString();
+        }
+
+        private static void GetAmountAtNode(string node, int amount, Dictionary<string, int> nodes, List<Connection> connections)
+        {
+            if (node == "ORE")
+            {
+                UsedOre += amount;
+                return;
+            }
+            if (nodes[node] >= amount)
+            {
+                nodes[node] -= amount;
+                return;
+            }
+            var neededAmount = amount - nodes[node]; 
+            var neededConnections = connections.Where(x => x.To == node).ToList();
+            foreach(var c in neededConnections)
+            {
+                var numberOfCycles = (int)Math.Ceiling((double)neededAmount / c.ToAmount);
+                var needed = c.FromAmount * numberOfCycles;
+                GetAmountAtNode(c.From, needed, nodes, connections);
+                var wasted = c.ToAmount * numberOfCycles - neededAmount;
+                nodes[node] = wasted;
+            }
+        }
+
+        public static string B(string input)
+        {
+            UsedOre = 0;
+            var rows = input.Replace("\r", "").Split('\n');
+            var recipes = new List<Recipe>();
+
+            foreach (var row in rows)
+            {
+                var split = row.Split(new[] { "=>" }, StringSplitOptions.RemoveEmptyEntries);
+                var froms = split[0].Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                var toInfo = split[1].Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                var toNode = toInfo[1];
+                var toAmount = int.Parse(toInfo[0]);
+
+                var recipe = new Recipe(toNode, toAmount);
+
+                foreach (var from in froms)
+                {
+                    var fromInfo = from.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    var fromNode = fromInfo[1];
+                    var fromAmount = int.Parse(fromInfo[0]);
+                    recipe.Ingredients.Add((fromNode, fromAmount));
+                }
+                recipes.Add(recipe);
+            }
+
+
+            var result = NeededOre(1, recipes);
+            return result.ToString();
+        }
+
+        public static int NeededOre(int amountOfFuel, List<Recipe> recipes)
+        {
+            var neededAmount = recipes.Select(x => x.Type).ToDictionary(x => x, x => 0);
+
+            var leftOvers = recipes.Select(x => x.Type).ToDictionary(x => x, x => 0);
+
+            neededAmount["FUEL"] = amountOfFuel;
+
+            var usedOre = 0;
+            while(neededAmount.Any(x => x.Value > 0))
+            {
+                var neededRecipe = neededAmount.FirstOrDefault(x => x.Value > 0);
+                usedOre += UpdateRequirements(neededRecipe.Key, neededRecipe.Value, recipes, neededAmount, leftOvers);
+            }
+            return usedOre;
+        }
+
+        public static int UpdateRequirements(string type, int amount, List<Recipe> recipes, Dictionary<string, int> neededAmounts, Dictionary<string, int> leftOvers)
+        {
+            if (type == "ORE")
+                return amount;
+            
+            if (leftOvers[type] >= amount)
+            {
+                neededAmounts[type] -= amount;
+                leftOvers[type] -= amount;
+                return 0;
+            }
+            else if (leftOvers[type] > 0)
+            {
+                amount = amount - leftOvers[type];
+                leftOvers[type] = 0;
+            }
+            
+            var recipe = recipes.First(x => x.Type == type);
+            var numOfReps = (int)Math.Ceiling((double)amount / recipe.Amount);
+            leftOvers[type] = (recipe.Amount - amount - leftOvers[type]) % recipe.Amount;
+            neededAmounts[type] = 0;
+            foreach (var i in recipe.Ingredients)
+            {
+                if (i.Item1 == "ORE")
+                    return i.Item2;
+                    
+                neededAmounts[i.Item1] = i.Item2 * numOfReps;
+            }
+            
+            return 0;
+        }
+
+        public class Recipe
+        {
+            public string Type { get; set; }
+            public int Amount { get; set; }
+            public List<(string, int)> Ingredients { get; set; } = new List<(string, int)>();
+
+            public Recipe(string type, int amount)
+            {
+                Type = type;
+                Amount = amount;
+            }
+
+            public override string ToString()
+            {
+                return $"Type: {Type}, Amount: {Amount}, NumIngredients: {Ingredients.Count}";
+            }
+        }
+
+        public class Connection
+        {
+            public string From { get; set; }
+            public string To { get; set; }
+            public int FromAmount { get; set; }
+            public int ToAmount { get; set; }
+
+            public Connection(string from, string to, int fromAmount, int toAmount)
+            {
+                From = from;
+                To = to;
+                FromAmount = fromAmount;
+                ToAmount = toAmount;
+            }
+
+            public override string ToString()
+            {
+                return $"From: {From}, FromAmount: {FromAmount}, To: {To}, ToAmount: {ToAmount}";
+            }
+        }
     }
 }
---
+++ AoC2019/Solutions/Day15.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AoC2019.Solutions
+{
+    public class Day15
+    {
+        public const int Height = 50;
+        public const int Width = 50;
+        public static char[,] Grid = new char[Width, Height];
+        public static HashSet<(int, int)> DeadEnds = new HashSet<(int, int)>();
+        public static (int, int) OxygenTank;
+        public static string A(string input)
+        {
+            var program = input.Split(',').Select(long.Parse).ToList();
+            var computer = new IntCodeComputer(program);
+            var computerThread = new Thread(f => { computer.Run(); });
+            var programThread = new Thread(f => { HandleLogic(computer.InputQueue, computer.OutputQueue); });
+            DeadEnds.Clear();
+
+
+            computerThread.Start();
+            programThread.Start();
+            
+            programThread.Join();
+            computerThread.Abort();
+
+            var openPaths = 0;
+            for(var y = 0; y < Height; y++)
+            {
+                for(var x = 0; x < Width; x++)
+                {
+                    Console.Write(Grid[x, y]);
+                    if (Grid[x, y] == '.')
+                        openPaths++;
+                }
+                Console.WriteLine();
+            }
+
+            var result = openPaths - DeadEnds.Count;
+            return result.ToString();
+        }
+
+        public static string B(string input)
+        {
+            var program = input.Split(',').Select(long.Parse).ToList();
+            var computer = new IntCodeComputer(program);
+            var computerThread = new Thread(f => { computer.Run(); });
+            var programThread = new Thread(f => { HandleLogic(computer.InputQueue, computer.OutputQueue); });
+            DeadEnds.Clear();
+
+
+            computerThread.Start();
+            programThread.Start();
+
+            programThread.Join();
+            computerThread.Abort();
+
+            var oxygenFront = new List<(int, int)> { OxygenTank };
+            var count = 0;
+            while (oxygenFront.Any())
+            {
+                var newFront = new List<(int, int)>();
+                foreach(var o in oxygenFront)
+                {
+                    Grid[o.Item1, o.Item2] = 'O';
+                    UpdateOxygenFront(o.Item1, o.Item2, newFront);
+                }
+                oxygenFront = newFront;
+                count++;
+            }
+            //for (var y = 0; y < Height; y++)
+            //{
+            //    for (var x = 0; x < Width; x++)
+            //    {
+            //        Console.Write(Grid[x, y]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            return (count-1).ToString();
+        }
+
+        private static void UpdateOxygenFront(int x, int y, List<(int, int)> currentFront)
+        {
+            var north = (x, y: y - 1);
+            var east = (x: x + 1, y);
+            var south = (x, y: y + 1);
+            var west = (x: x - 1, y);
+
+            if(Grid[north.x, north.y] == '.')
+            {
+                Grid[north.x, north.y] = 'O';
+                currentFront.Add(north);
+            }
+            if (Grid[east.x, east.y] == '.')
+            {
+                Grid[east.x, east.y] = 'O';
+                currentFront.Add(east);
+            }
+            if (Grid[south.x, south.y] == '.')
+            {
+                Grid[south.x, south.y] = 'O';
+                currentFront.Add(south);
+            }
+            if (Grid[west.x, west.y] == '.')
+            {
+                Grid[west.x, west.y] = 'O';
+                currentFront.Add(west);
+            }
+        }
+
+        private static void HandleLogic(BlockingCollection<long> inputQueue, BlockingCollection<long> outputQueue)
+        {
+            var x = Width / 2;
+            var y = Height / 2;
+            var direction = 1;
+            while (true)
+            {
+                direction = ChooseDirection(x, y, direction);
+                if (direction == -1)
+                    return;
+                inputQueue.Add(direction);
+
+                var output = outputQueue.Take();
+
+                switch (output)
+                {
+                    case 0:
+                        if (direction == 1)
+                        {
+                            Grid[x, y - 1] = '#';
+                        } else if(direction == 2)
+                        {
+                            Grid[x, y + 1] = '#';
+                        }else if(direction == 3)
+                        {
+                            Grid[x - 1, y] = '#';
+                        }
+                        else
+                        {
+                            Grid[x + 1, y] = '#';
+                        }
+                        break;
+                    case 1:
+                        Grid[x, y] = '.';
+                        switch (direction)
+                        {
+                            case 1:
+                                y -= 1;
+                                break;
+                            case 2:
+                                y += 1;
+                                break;
+                            case 3:
+                                x -= 1;
+                                break;
+                            case 4:
+                                x += 1;
+                                break;
+                        }
+                        break;
+                    case 2:
+                        Grid[x, y] = '.';
+                        switch (direction)
+                        {
+                            case 1:
+                                y -= 1;
+                                break;
+                            case 2:
+                                y += 1;
+                                break;
+                            case 3:
+                                x -= 1;
+                                break;
+                            case 4:
+                                x += 1;
+                                break;
+                        }
+                        OxygenTank = (x, y);
+                        break;
+                        //case 2:
+                        //    Grid[x, y] = '.';
+                        //    switch (direction)
+                        //    {
+                        //        case 1:
+                        //            y -= 1;
+                        //            break;
+                        //        case 2:
+                        //            y += 1;
+                        //            break;
+                        //        case 3:
+                        //            x -= 1;
+                        //            break;
+                        //        case 4:
+                        //            x += 1;
+                        //            break;
+                        //    }
+                        //    Grid[x, y] = 'O';
+                        //    return;
+                }
+                //Grid[x, y] = 'o';
+                //for (var yTemp = 0; yTemp < Height; yTemp++)
+                //{
+                //    for (var xTemp = 0; xTemp < Width; xTemp++)
+                //    {
+                //        Console.Write(Grid[xTemp, yTemp]);
+                //    }
+                //    Console.WriteLine();
+                //}
+                //Console.WriteLine("############################");
+            }
+        }
+
+        private static int ChooseDirection(int x, int y, int lastDirection)
+        {
+            if (Grid[x, y-1] == default(char))
+                return 1;
+            if (Grid[x+1, y] == default(char))
+                return 4;
+            if (Grid[x, y+1] == default(char))
+                return 2;
+            if (Grid[x-1, y] == default(char))
+                return 3;
+
+            UpdateDeadEnds(x, y);
+            var options = GetOptions(x, y);
+            if (options.Count > 1)
+                return options.First(o => o != lastDirection);
+            else if (options.Count == 1)
+                return options.First();
+            else
+                return -1;
+        }
+
+        private static List<int> GetOptions(int x, int y)
+        {
+            //var options = new List<int>();
+            //if (Grid[x, y - 1] == '.')
+            //    options.Add(1);
+            //if (Grid[x + 1, y] == '.')
+            //    options.Add(4);
+            //if (Grid[x, y + 1] == '.')
+            //    options.Add(2);
+            //if (Grid[x - 1, y] == '.')
+            //    options.Add(3);
+
+            var options = new List<int>();
+            var north = (x, y: y - 1);
+            var east = (x: x + 1, y);
+            var south = (x, y: y + 1);
+            var west = (x: x - 1, y);
+            if (Grid[north.x, north.y] == '.' && !DeadEnds.Contains(north))
+                options.Add(1);
+            if (Grid[east.x, east.y] == '.' && !DeadEnds.Contains(east))
+                options.Add(4);
+            if (Grid[south.x, south.y] == '.' && !DeadEnds.Contains(south))
+                options.Add(2);
+            if (Grid[west.x, west.y] == '.' && !DeadEnds.Contains(west))
+                options.Add(3);
+            return options;
+        }
+        private static void UpdateDeadEnds(int x, int y)
+        {
+            var options = new List<int>();
+            var north = (x, y: y - 1);
+            var east = (x: x + 1, y);
+            var south = (x, y: y +1);
+            var west = (x: x - 1, y);
+            if (Grid[north.x, north.y] == '#' || DeadEnds.Contains(north))
+                options.Add(1);
+            if (Grid[east.x, east.y] == '#' || DeadEnds.Contains(east))
+                options.Add(4);
+            if (Grid[south.x, south.y] == '#' || DeadEnds.Contains(south))
+                options.Add(2);
+            if (Grid[west.x, west.y] == '#' || DeadEnds.Contains(west))
+                options.Add(3);
+
+            if (options.Count >= 3)
+                DeadEnds.Add((x, y));
+        }
+    }
+}
