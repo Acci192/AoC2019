3b9304a6ec37d40fa3cd7e86aa5e64dc137a5490
Acci192 <k.andreas@live.se>
2019-12-17 20:10:20 +0100

    Day 17

--- AoC2019/Solutions/Day17.cs
+++ AoC2019/Solutions/Day17.cs
@@ -11,63 +11,76 @@ namespace AoC2019.Solutions
     public class Day17
     {
         public static char[,] Grid = new char[75, 75];
+        public static string Result_A = string.Empty;
+        public static string Result_B = string.Empty;
         public static string A(string input)
         {
             var program = input.Split(',').Select(long.Parse).ToList();
             var computer = new IntCodeComputer(program);
             var computerThread = new Thread(f => { computer.Run(); });
-            var programThread = new Thread(f => { HandleLogic(computer.InputQueue, computer.OutputQueue); });
-
+            var programThread = new Thread(f => { HandleLogic_A(computer.InputQueue, computer.OutputQueue); });
+            
             computerThread.Start();
             programThread.Start();
 
             computerThread.Join();
             programThread.Join();
-            //programThread.Abort();
-
-            //var openPaths = 0;
-            //for (var y = 0; y < Height; y++)
-            //{
-            //    for (var x = 0; x < Width; x++)
-            //    {
-            //        if (Grid[x, y] == '.')
-            //            openPaths++;
-            //    }
-            //}
-            
-            return string.Empty;
+            return Result_A;
         }
 
-        private static void HandleLogic(BlockingCollection<long> inputQueue, BlockingCollection<long> outputQueue)
+        public static string B(string input)
+        {
+            var program = input.Split(',').Select(long.Parse).ToList();
+            var computer = new IntCodeComputer(program);
+            var computerThread = new Thread(f => { computer.Run(); });
+            var programThread = new Thread(f => { HandleLogic_B(computer.InputQueue, computer.OutputQueue); });
+            
+            computer.SetValueAt(0, 2);
+            computerThread.Start();
+            programThread.Start();
+
+            computerThread.Join();
+            programThread.Join();
+
+            return Result_B;
+        }
+
+        private static void HandleLogic_A(BlockingCollection<long> inputQueue, BlockingCollection<long> outputQueue)
         {
             var x = 1;
             var y = 1;
             var width = 0;
+            var count = 0;
+
+            var lastChar = (char)0;
             while (true)
             {
                 var output = outputQueue.Take();
+                
                 if (output == long.MinValue)
                     break;
                 if (output == 10)
                 {
                     y++;
-                    if(width == 0)
+                    if (width == 0)
                         width = x;
                     x = 1;
                     continue;
                 }
+                if (lastChar == 10 && output == 10)
+                    y = 0;
+                lastChar = (char)output;
+
                 Grid[x, y] = (char)output;
                 x++;
+
             }
 
             var height = y;
-
-
-
             var intersections = new List<(int, int)>();
-            for(var gy = 1; gy < height; gy++)
+            for (var gy = 1; gy < height; gy++)
             {
-                for(var gx = 1; gx < width; gx++)
+                for (var gx = 1; gx < width; gx++)
                 {
                     if (Grid[gx, gy - 1] == '#'
                         && Grid[gx, gy + 1] == '#'
@@ -75,23 +88,30 @@ namespace AoC2019.Solutions
                         && Grid[gx + 1, gy] == '#'
                         && Grid[gx, gy] == '#')
                         intersections.Add((gx, gy));
-                    Console.Write(Grid[gx, gy]);
+                    //Console.Write(Grid[gx, gy]);
                 }
-                Console.WriteLine();
+                //Console.WriteLine();
             }
 
-            var sum = 0;
-            foreach(var i in intersections)
-            {
-                sum += (i.Item1 - 1) * (i.Item2 - 1);
-            }
-            Console.WriteLine(sum);
+            Result_A = intersections.Select(i => (i.Item1 - 1) * (i.Item2 - 1)).Sum().ToString();
         }
 
-        public static string B(string input)
+        private static void HandleLogic_B(BlockingCollection<long> inputQueue, BlockingCollection<long> outputQueue)
         {
-            var rows = input.Split('\n');
-            return string.Empty;
+            var input = "A,B,A,C,C,A,B,C,B,B\nL,8,R,10,L,8,R,8\nL,12,R,8,R,8\nL,8,R,6,R,6,R,10,L,8\nn\n";
+            input.ToList().ForEach(c => inputQueue.Add(c));
+            
+            while (true)
+            {
+                var output = outputQueue.Take();
+                if (output > char.MaxValue)
+                {
+                    Result_B = output.ToString();
+                    return;
+                }
+
+                //Console.Write((char)output);
+            }
         }
     }
 }
